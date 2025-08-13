e499df043f87f96c55842cc7667b7fbb21fc2c03
labp1 <labp.work@gmail.com>
2025-08-13 14:31:00 +1000

    fix(gitignore): ignore node_modules folder, remove node_modules cache

--- Week4/server.js
+++ Week4/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 var port = process.env.port || 3000;
 app.listen(port, () => {
-    console.log("App listening to: " + port)
+    console.log("App listening to: http://localhost" + port)
 })
 
 // Connect to MongoDB database
